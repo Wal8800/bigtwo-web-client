79407b37b61d4beeb2d76acf3a61b3eeb96a004c
John Law <8260377+Wal8800@users.noreply.github.com>
2023-07-28 23:04:42 +1200

    Implement reset and start with rendering

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import * as PIXI from "pixi.js";
 import { Sprite, Stage } from "@pixi/react";
 import Hand, { MAX_HAND_WIDTH } from "./components/hand/Hand";
-import { loadPyodide } from "pyodide";
+import { PyodideInterface, loadPyodide } from "pyodide";
+import type { PyCallable, PyProxy, PyProxyWithGet } from "pyodide/ffi";
+import * as tf from "@tensorflow/tfjs";
 import ClipLoader from "react-spinners/ClipLoader";
 import { PyCard, toPyCard } from "./model";
 import OpponentHand from "./components/hand/OpponentHand";
@@ -15,13 +17,76 @@ const STAGE_WIDTH = 1280;
 const STAGE_HEIGHT = 720;
 const STAGE_PADDING = 50;
 
+const DEFAULT_PLAYER_ID = 0;
+
+type PyEnv = {
+  game: PyProxy;
+  obsToOhe: PyCallable;
+  generateActionMask: PyCallable;
+  rawActionToCat: PyProxyWithGet;
+  catToRawAction: PyProxyWithGet;
+};
+
+const predictAction = function (
+  model: tf.GraphModel,
+  inputs: number[],
+  mask: number[]
+): any {
+  const inputTensor = tf.tensor(inputs).reshape([1, -1]);
+
+  // The model returns two output tensor in an array.
+  const output = model.predict({
+    observations: inputTensor,
+  }) as tf.Tensor<tf.Rank>[];
+
+  const infMask = tf.tensor(1).sub(tf.tensor(mask)).mul(tf.scalar(-1e9));
+  const finalOutput = output[0].squeeze().add(infMask);
+
+  const cat = finalOutput.argMax().arraySync();
+  return cat;
+};
+
+const resetAndStart = async function (pyEnv: PyEnv, model: any) {
+  let obs = pyEnv.game.reset();
+
+  while (true) {
+    if (obs.current_player === DEFAULT_PLAYER_ID) {
+      // return the player hands and number of opponent cards
+      return obs;
+    }
+
+    const ohe = pyEnv.obsToOhe(obs).tolist();
+    const mask = pyEnv.generateActionMask(pyEnv.rawActionToCat, obs);
+
+    const cat = predictAction(
+      model,
+      ohe.toJs({ create_proxies: false }),
+      mask.toJs({ create_proxies: false })
+    );
+    const rawAction = pyEnv.catToRawAction.get(cat);
+    pyEnv.game.step(rawAction);
+
+    obs = pyEnv.game.get_current_player_obs();
+  }
+};
+
 function App() {
   const [loading, setLoading] = useState(true);
-  const [pyodideRunTime, setPyRunTime] = useState<any | undefined>(undefined);
-  const [hands, setHands] = useState<Array<Array<PyCard>>>([]);
+  const [pyodideRunTime, setPyRunTime] = useState<PyodideInterface>();
+  const [hand, setHand] = useState<Array<PyCard>>([]);
+  const [model, setModel] = useState<tf.GraphModel>();
+  const [pyEnv, setPyEnv] = useState<PyEnv>();
+
+  const [numOpponentCard, setNumOpponentCards] = useState<Array<number>>([
+    13, 13, 13,
+  ]);
+  const [lastPlayedCards, setLastPlayed] = useState<Array<PyCard>>([]);
 
   useEffect(() => {
     const loadApplication = async () => {
+      const model = await tf.loadGraphModel("webmodel/model.json");
+      setModel(model);
+
       const runTime = await loadPyodide({
         indexURL: "https://cdn.jsdelivr.net/pyodide/v0.23.4/full/",
       });
@@ -41,29 +106,65 @@ function App() {
             print("loaded card games")
         `);
 
+      // Initialising global python variables that we will be using.
       await runTime.runPythonAsync(`
         from bigtwo.bigtwo import BigTwo
+        from bigtwo.preprocessing import (
+          create_action_cat_mapping,
+          generate_action_mask,
+          obs_to_ohe,
+        )
 
         game = BigTwo()
+        cat_to_raw_action, raw_action_to_cat = create_action_cat_mapping()
       `);
 
-      const cards = runTime.globals.get("game").player_hands.get(0).cards;
+      const game = runTime.globals.get("game");
+      const catToRawAction = runTime.globals.get("cat_to_raw_action");
+      const rawActionToCat = runTime.globals.get("raw_action_to_cat");
+      const generateActionMask = runTime.globals.get("generate_action_mask");
+      const obsToOhe = runTime.globals.get("obs_to_ohe");
 
+      const pyEnv = {
+        game,
+        catToRawAction,
+        rawActionToCat,
+        generateActionMask,
+        obsToOhe,
+      };
+      setPyEnv(pyEnv);
+
+      const obs = await resetAndStart(pyEnv, model);
+
+      const cards = obs.your_hands.cards;
       const transformedCards: PyCard[] = [];
       for (let card of cards) {
         transformedCards.push(toPyCard(card.suit.value, card.rank.value));
       }
 
-      const hands: PyCard[][] = [];
-      hands.push(transformedCards);
-      setHands(hands);
+      const lastPlayedCards: PyCard[] = [];
+      for (let card of obs.last_cards_played) {
+        lastPlayedCards.push(toPyCard(card.suit.value, card.rank.value));
+      }
+
+      setNumOpponentCards(
+        obs.num_card_per_player.toJs({ create_proxies: false })
+      );
+      setLastPlayed(lastPlayedCards);
+
+      setHand(transformedCards);
       setLoading(false);
+
+      // Clean up any PyProxy we don't need
+      cards.destroy();
+      obs.destroy();
     };
 
     loadApplication();
   }, []);
 
-  const bunny = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png";
+  const bunny =
+    "https://s3-us-west-2.amazonaws.com/s.cdpn.io/693612/IaUrttj.png";
 
   return (
     <div>
@@ -81,26 +182,33 @@ function App() {
           height={STAGE_HEIGHT}
           options={{ backgroundColor: 0x5f92de }}
         >
+          {/* last played cards */}
+          <Hand
+            x={(STAGE_WIDTH - MAX_HAND_WIDTH) / 2}
+            y={STAGE_HEIGHT / 2 - CARD_HEIGHT / 2}
+            cards={lastPlayedCards}
+          />
+
           {/* player hand */}
           <Hand
             x={(STAGE_WIDTH - MAX_HAND_WIDTH) / 2}
             y={STAGE_HEIGHT - CARD_HEIGHT - STAGE_PADDING}
-            cards={hands[0]}
+            cards={hand}
           />
 
           {/* left hand */}
           <OpponentHand
             x={STAGE_PADDING + CARD_HEIGHT}
             y={(STAGE_HEIGHT - MAX_HAND_WIDTH) / 2}
-            numCards={13}
+            numCards={numOpponentCard[0]}
             rotation={90}
           />
 
           {/* top hand */}
           <OpponentHand
-            x={((STAGE_WIDTH - MAX_HAND_WIDTH) / 2) + MAX_HAND_WIDTH}
+            x={(STAGE_WIDTH - MAX_HAND_WIDTH) / 2 + MAX_HAND_WIDTH}
             y={CARD_HEIGHT + STAGE_PADDING}
-            numCards={13}
+            numCards={numOpponentCard[1]}
             rotation={180}
           />
 
@@ -108,12 +216,13 @@ function App() {
           <OpponentHand
             x={STAGE_WIDTH - STAGE_PADDING - CARD_HEIGHT}
             y={STAGE_PADDING + MAX_HAND_WIDTH}
-            numCards={13}
+            numCards={numOpponentCard[2]}
             rotation={270}
           />
 
-        <Sprite
-            x={((STAGE_WIDTH - MAX_HAND_WIDTH) / 2) + MAX_HAND_WIDTH + 25}
+          {/* play button */}
+          <Sprite
+            x={(STAGE_WIDTH - MAX_HAND_WIDTH) / 2 + MAX_HAND_WIDTH + 25}
             y={STAGE_HEIGHT - CARD_HEIGHT - STAGE_PADDING}
             anchor={[0.5, 0.5]}
             interactive={true}
